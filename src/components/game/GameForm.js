794bca5417aabdfeae2a3f07ded642e9506dee19
Loganwebb1994 <loganwebb1994@gmail.com>
2021-05-12 10:51:43 -0500

    gameform works

--- src/components/game/GameForm.js
+++ src/components/game/GameForm.js
@@ -4,85 +4,110 @@ import { useHistory, useParams } from 'react-router-dom'
 
 
 export const GameForm = () => {
-  const history = useHistory()
-  const { createGame, getGameTypes, gameTypes, getGameById, updateGame } = useContext(GameContext)
-  const {gameId} = useParams()
-  const [currentGame, setCurrentGame] = useState({
-      name: "",
-      game_type_id: 0
-  })
-  useEffect(() => {
-      getGameTypes().then(()=>{
-          if(gameId){
-              getGameById(gameId).then(setCurrentGame)
-          }
-      })
-  }, [])
+    debugger
+    const history = useHistory()
+    
+    const { createGame, getGameTypes, gameTypes, getGameById, updateGame } = useContext(GameContext)
+    
+    const {gameId} = useParams()
+    
+    const [currentGame, setCurrentGame] = useState({
+        title: "",
+        game_type_id: 0
+    })
+    useEffect(() => {
+        getGameTypes().then(()=>{
+            if(gameId){
+                getGameById(gameId).then(setCurrentGame)
+            }
+        })
+    }, [])
 
-  const handleInputChange = (e) => {
-      const tempGame = {...currentGame}
-      tempGame[e.target.name] = e.target.value
-      setCurrentGame(tempGame)
-  }
+    const handleInputChange = (e) => {
+        const tempGame = {...currentGame}
+        tempGame[e.target.name] = e.target.value
+        setCurrentGame(tempGame)
+    }
 
-    return (
-      <form className="gameForm">
-      <h2 className="gameForm__title">Register New Game</h2>
-      <fieldset>
-          <div className="form-group">
-              <label htmlFor="name">Name: </label>
-              <input type="text" name="name" required autoFocus className="form-control"
-                  value={currentGame.name}
-                  onChange={handleInputChange}
-              />
-          </div>
-      </fieldset>
-      <fieldset>
-          <div className="form-group">
-              <label htmlFor="game_type_id">GameType: </label>
-              <select name="game_type_id" className="form-control"
-                  value={currentGame.game_type_id}
-                  onChange={handleInputChange}>
-                  <option value="0">Select a Game Type</option>
-                  {gameTypes.map(gameType => (
-                      <option key={gameType.id} value={gameType.id}>
-                          {gameType.name}
-                      </option>
-                  ))}
-              </select>
-          </div>
-      </fieldset>
+        return (
+        <form className="gameForm">
+        <h2 className="gameForm__title">Register New Game</h2>
+        <fieldset>
+            <div className="form-group">
+                <label htmlFor="title">Title: </label>
+                <input type="text" name="title" required autoFocus className="form-control"
+                    value={currentGame.title}
+                    onChange={handleInputChange}
+                />
+            </div>
+        </fieldset>
+        <fieldset>
+            <div className="form-group">
+                <label htmlFor="maker">Maker: </label>
+                <input type="text" name="maker" required autoFocus className="form-control"
+                    value={currentGame.maker}
+                    onChange={handleInputChange}
+                />
+            </div>
+        </fieldset>
+        <fieldset>
+            <div className="form-group">
+                <label htmlFor="numberOfPlayers">Number Of Players: </label>
+                <input type="text" name="numberOfPlayers" required autoFocus className="form-control"
+                    value={currentGame.numberOfPlayers}
+                    onChange={handleInputChange}
+                />
+            </div>
+        </fieldset>
+        <fieldset>
+            <div className="form-group">
+                <label htmlFor="skillLevel">Skill Level: </label>
+                <input type="text" name="skillLevel" required autoFocus className="form-control"
+                    value={currentGame.skillLevel}
+                    onChange={handleInputChange}
+                />
+            </div>
+        </fieldset>
+        <fieldset>
+            <div className="form-group">
+                <label htmlFor="game_type_id">GameType: </label>
+                <select name="game_type_id" className="form-control"
+                    value={currentGame.game_type_id}
+                    onChange={handleInputChange}>
+                    <option value="0">Select a Game Type</option>
+                    {gameTypes.map(gameType => (
+                        <option key={gameType.id} value={gameType.id}>
+                            {gameType.label}
+                        </option>
+                    ))}
+                </select>
+            </div>
+        </fieldset>
 
 
 
-      {/* You create the rest of the input fields for each game property */}
 
-      <button type="submit"
-          onClick={evt => {
-              // Prevent form from being submitted
-              evt.preventDefault()
+        {/* You create the rest of the input fields for each game property */}
 
-              const game = {
-                  name: currentGame.name,
-                  gameTypeId: parseInt(currentGame.game_type_id)
-              }
-              // Send POST request to your API
-              if(gameId){
-                  const game = {
-                      id: currentGame.id,
-                      gameTypeId: parseInt(currentGame.game_type_id),
-                      name: currentGame.name
-                  }
-                  updateGame(game).then(() => history.push("/"))
-              }else{
-                  const game = {
-                      name: currentGame.name,
-                      gameTypeId: parseInt(currentGame.game_type_id)
-                  }
-                  createGame(game).then(() => history.push("/"))
-              }
-          }}
-          className="btn btn-primary">{gameId?"Edit":"Create"}</button>
-  </form>
-    )
-}
+        <button type="submit"
+            onClick={evt => {
+                evt.preventDefault()
+                const game = 
+                // Prevent form from being submitted
+                {
+                    title: currentGame.title,
+                    maker: currentGame.maker,
+                    numberOfPlayers: parseInt(currentGame.numberOfPlayers),
+                    skillLevel: parseInt(currentGame.skillLevel),
+                    game_type_id: parseInt(currentGame.game_type_id)
+
+                }
+
+                // Send POST request to your API
+                    createGame(game).then(() => history.push("/"))
+                }
+            }
+            className="btn btn-primary">{gameId?"Edit":"Create"}</button>
+    </form>
+        )
+    }
